45068e731ad4db9133c67013bca7e63d0f699309
Suz Hinton <suz.hinton@gmail.com>
2014-08-26 21:12:19 -0700

    add test entry to db, add uuid dep to package.json

--- leveldb.js
+++ leveldb.js
@@ -5,21 +5,37 @@ var levelup = require('levelup')
 var db = levelup('./ps-db')
 
 // 2) put a key & value
-db.put('name', 'LevelUP', function (err) {
-  if (err) return console.log('Ooops!', err) // some kind of I/O error
+// db.put('name', 'LevelUP', function (err) {
+//   if (err) return console.log('Ooops!', err) // some kind of I/O error
 
-  // 3) fetch by key
-//   db.get('name', function (err, value) {
-//     if (err) return console.log('Ooops!', err) // likely the key was not found
+//   // 3) fetch by key
+// //   db.get('name', function (err, value) {
+// //     if (err) return console.log('Ooops!', err) // likely the key was not found
 
-//   })
+// //   })
+// // })
+
+// //   db.put('hello', 'is it me you\'re looking for?', function() {
+
+// //     db.createReadStream()
+// //     .on('data', function (data) {
+// //       console.log(data.key, '=', data.value)
+// //     });
+  
 // })
 
-//   db.put('hello', 'is it me you\'re looking for?', function() {
+// db.batch()
+//   .del('name')
+//   .put('job~1~email', 'suz@noopkat.com')
+//   .put('job~1~files', 'thing:13505,thing:152484')
+//   .put('job~1~message', 'only the first file for 8903 please')
+//   .put('job~1~status', 'pending')
+//   .put('job~1~printer', 'TAZ-1')
+//   .put('printer~1~port', '5000')
+//   .put('printer~1~name', 'TAZ-1')
+//   .write(function () { console.log('Done!') });
 
-//     db.createReadStream()
+//   db.createReadStream()
 //     .on('data', function (data) {
 //       console.log(data.key, '=', data.value)
 //     });
-  
-})
